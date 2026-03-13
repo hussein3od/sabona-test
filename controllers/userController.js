6b335b541a61d048a6c54f2e45300e932186fad5
hussein <hhssing0@gmail.com>
2026-03-13 14:39:06 +0300

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,43 @@ import pool from "../db.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+export const getAll = async (req, res) => {
+  try {
+    const checkUser = await pool.query(
+      "SELECT * FROM users"
+    );
+
+    if (checkUser.rows.length > 0) {
+      return res.status(400).json({ message: "No Users Founf" });
+    }
+
+    const result = await pool.query(
+      `INSERT INTO users (name, phone, password, role, is_active, created_at)
+       VALUES ($1, $2, $3, $4, $5, NOW())
+       RETURNING id, name, phone, role`,
+      [name, phone, hashedPassword, "user", true]
+    );
+
+    const user = result.rows[0];
+
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(201).json({
+      message: "User created successfully",
+      user,
+      token,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 export const register = async (req, res) => {
   try {
     const { name, phone, password } = req.body;
@@ -68,7 +105,6 @@ export const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid phone or password" });
 
-    // 🔥 توليد JWT
     const token = jwt.sign(
       { userId: user.id, role: user.role },
       process.env.JWT_SECRET,
